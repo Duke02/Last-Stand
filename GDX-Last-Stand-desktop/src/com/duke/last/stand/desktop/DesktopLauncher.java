5d5dcd2b3c5b2e65c1cc93cb8c76496180a82960
The Duke <tkmshere@yahoo.com>
2015-08-27 22:18:52 -0400

    Just starting it out.

---
+++ GDX-Last-Stand-desktop/src/com/duke/last/stand/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.duke.last.stand.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.duke.last.stand.LastStand;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LastStand(), config);
+	}
+}
